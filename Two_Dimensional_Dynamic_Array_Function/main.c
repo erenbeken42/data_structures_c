9a49f3698ee5af3dad254bc31d466b8cc0aea30c
Eren Beken <119003277+erenbeken42@users.noreply.github.com>
2023-11-15 21:51:46 +0300

    Update main.c

--- Two_Dimensional_Dynamic_Array_Function/main.c
+++ Two_Dimensional_Dynamic_Array_Function/main.c
@@ -4,25 +4,25 @@
 int main()
 {
   int**ptr;
-  int satir_sayisi,sutun_sayisi;
-  scanf("%d",&satir_sayisi);
-  scanf("%d",&sutun_sayisi);
+  int number_lines,number_columns;
+  scanf("%d",&number_lines);
+  scanf("%d",&number_columns);
 
 
- iki_boyutlu_matris_olustur(&ptr,satir_sayisi,sutun_sayisi);
+ create_two_dimensional_array(&ptr,number_lines,number_columns);
 
 
     return 0;
 }
 
 
-  void iki_boyutlu_matris_olustur(int*** matris,int satir_sayisi, int sutun_sayisi){
+  void create_two_dimensional_array(int*** matris,int number_lines, int number_columns){
   int i,j;
-  *matris=(int**)malloc(sizeof(int*)* satir_sayisi);
-  for(i=0; i<satir_sayisi; i++)
+  *matris=(int**)malloc(sizeof(int*)* number_lines);
+  for(i=0; i<number_lines; i++)
   {
-      *(*matris+i)=(int*)malloc(sizeof(int)* sutun_sayisi);
-      for(j=0; j<sutun_sayisi;j++)
+      *(*matris+i)=(int*)malloc(sizeof(int)* snumber_columns);
+      for(j=0; j<number_columns;j++)
       {
          *(*(*matris+i)+j)=rand()%100;
          printf("%d ",*(*(*matris+i)+j) );
